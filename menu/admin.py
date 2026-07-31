770b6968fe1f60d1cf2113cd69b381653993684a
Amir <amirrezarejaie@gmail.com>
2026-07-31 13:20:36 +0430

    Add food model and improve menu admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,81 @@
 from django.contrib import admin
-from .models import Category
+
+from .models import Category, Food
 
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "is_active",
+        "display_order",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_active",
+    )
+
+    search_fields = (
+        "name",
+        "description",
+    )
+
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
+
+    list_editable = (
+        "is_active",
+        "display_order",
+    )
+
+    ordering = (
+        "display_order",
+        "name",
+    )
+
+
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "category",
+        "price",
+        "is_available",
+        "is_featured",
+        "display_order",
+    )
+
+    list_filter = (
+        "category",
+        "is_available",
+        "is_featured",
+    )
+
+    search_fields = (
+        "name",
+        "short_description",
+        "description",
+        "ingredients",
+    )
+
+    prepopulated_fields = {
+        "slug": ("name",),
+    }
+
+    list_editable = (
+        "price",
+        "is_available",
+        "is_featured",
+        "display_order",
+    )
+
+    autocomplete_fields = (
+        "category",
+    )
+
+    ordering = (
+        "display_order",
+        "name",
+    )
