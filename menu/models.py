6831b83a4513db1f0589786d9be13fadbcf14122
Amir <amirrezarejaie@gmail.com>
2026-07-31 12:29:48 +0430

    Create category model with automatic slug generation

--- menu/models.py
+++ menu/models.py
@@ -1,9 +1,54 @@
 from django.db import models
+from django.utils.text import slugify
+
 from common.models import BaseModel
 
 
 class Category(BaseModel):
-    name = models.CharField(max_length=100)
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+        verbose_name="Category Name",
+    )
+
+    slug = models.SlugField(
+        unique=True,
+        blank=True,
+        verbose_name="Slug",
+    )
+
+    description = models.TextField(
+        blank=True,
+        verbose_name="Description",
+    )
+
+    image = models.ImageField(
+        upload_to="categories/",
+        blank=True,
+        null=True,
+        verbose_name="Category Image",
+    )
+
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name="Active",
+    )
+
+    display_order = models.PositiveIntegerField(
+        default=0,
+        verbose_name="Display Order",
+    )
+
+    class Meta:
+        ordering = ["display_order", "name"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
