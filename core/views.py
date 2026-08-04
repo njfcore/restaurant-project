89d2034d01e1b2baa5dd5c1a1388a66986fbe7ca
Amir <amirrezarejaie@gmail.com>
2026-08-04 15:36:39 +0430

    feat(frontend): complete home page with featured dishes and reusable components

--- core/views.py
+++ core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 
-from menu.models import Category
+from menu.models import Category, Food
 
 def home_view(request):
+
     categories = Category.objects.filter(
         is_active=True
     ).order_by(
@@ -10,8 +11,19 @@ def home_view(request):
         "name"
     )
 
+    featured_foods = Food.objects.filter(
+        is_available=True,
+        is_featured=True,
+    ).select_related(
+        "category"
+    ).order_by(
+        "display_order",
+        "name"
+    )
+
     context = {
         "categories": categories,
+        "featured_foods": featured_foods,
     }
 
     return render(
